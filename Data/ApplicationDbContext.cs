0a40b5b81d200754dfa4bfc176f22ad19579bee5
Shaina Sheethal M J <shainasmj@gmail.com>
2026-04-20 00:21:44 +0530

    Configured SQL Server connection and registered DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AssetIQ.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Tables will be added here later
+    }
+}
